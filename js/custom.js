37f24edddb4f9cfa620dc45a730ab84f22b013b0
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-01-24 23:59:14 +0100

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+/*
+    * load more btn loading spin toggle *
+*/
+
+const loadMoreBtn = document.querySelector("[data-load-more]");
+
+loadMoreBtn.addEventListener("click", function () {
+    this.classList.toggle("active");
+});
+
+// end of btn loading spin toggle
+
+
+// alert("Desktop only!✅  No mobiles allowed!🚫")
+// ❌🛑🆗
+
+
+
+
+// dark mode toggle
+
+// const checkbox = document.getElementById("checkbox")
+// checkbox.addEventListener("change", () => {
+    // document.body.classList.toggle("dark")
+// })
+
+// end of dark mode toggle
